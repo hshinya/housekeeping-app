b02e16e273a81308a1f3ddeeec6c40530196082c
shin <shinya.4n8.12623@gmail.com>
2024-07-17 08:02:26 +0900

    トランザクション更新のルート修正

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,8 @@ Route::get('/transactions',[TransactionController::class, 'index'])->name('trans
 Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
 Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
 Route::get('/transactions/{id}/edit', [TransactionController::class, 'edit'])->name('transactions.edit');
-Route::put('/transactions/{id}', [TransactionController::class, 'update'])->name('transactions.update');
+// Route::put('/transactions/{id}', [TransactionController::class, 'update'])->name('transactions.update');
+Route::post('/transactions/{id}', [TransactionController::class, 'update'])->name('transactions.update');
 Route::delete('/transactions/{id}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
 
 // レポートページ
