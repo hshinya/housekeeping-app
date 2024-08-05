f07d58eec725b47e863f516b9404398e1ed102a0
shin <shinya.4n8.12623@gmail.com>
2024-08-06 07:17:56 +0900

    ユーザー管理ページ

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/', function () {
     ]);
 });
 
+// ダッシュボード
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
